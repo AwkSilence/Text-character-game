b7c0325c253f2b5291254f8a3f40c5bea6ca0270
Greeblizzy <lisa.zheng@stonybrook.edu>
2018-09-14 20:52:07 -0400

    Removed all testing files

--- Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class deku {
-
-    public static void main (String[] args) {
-	// write your code here
-        System.out.println("I wanna be a hero too");
-    }
-}
