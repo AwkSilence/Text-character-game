aaa34f2c67c2a8f19dbab67038d13f5f35fb9595
Gregory Wong <gregory.wong.1@stonybrook.edu>
2018-09-14 20:40:55 -0400

    I wanna be a hero too.

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class deku {
+
+    public static void main (String[] args) {
+	// write your code here
+        System.out.println("I wanna be a hero too");
+    }
+}
