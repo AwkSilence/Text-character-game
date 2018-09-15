4cc14fa3b5a6821774ba98c024cc73676530b91b
chanjjessica <jessica.j.chan@stonybrook.edu>
2018-09-15 02:36:23 -0400

    message class

--- Hackathon2018/src/Message.java
+++ Hackathon2018/src/Message.java
@@ -1,4 +1,28 @@
-
+//implement into others
 public class Message {
+	private String message;
+	private int hungerVal;
+	private int moraleVal;
+	Message(String message, int hungerVal, int moraleVal){
+	}
+	public void setMessage(String message) {
+		this.message=message;
+	}
+	public String getMessage() {
+		return message;	
+	}
+	public void setHungerVal(int hungerVal) {
+		this.hungerVal= hungerVal;
+	}
+	public int getHungerVal() {
+		return hungerVal;
+	}
+	public void setMoraleVal(int moraleVal) {
+		this.moraleVal= moraleVal;
+	}
+	public int getMoraleVal() {
+		return moraleVal;
+	}
+	
 
 }
